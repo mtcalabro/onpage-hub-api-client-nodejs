a32f98f2e90df8e0cce1419249137f7f048ea76c
Andrew Ben <andrew.p.ben@gmail.com>
2015-03-26 11:21:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# onpage-hub-api-client-nodejs
-Node.js module to send Pages to OnPage recipients
+Node.js onpage-hub-api-client module is simple script to send the Page to OnPage recipients.
+
+Basic usage:
+
+#!/usr/bin/env node
+
+var OnPageHubApiClient = require('./index');
+// THE ENTERPRISE NAME AND TOKEN HAS TO BE REQUESTED FROM ONPAGE SUPPORT.
+var onPageHubApiClient = new OnPageHubApiClient('enterprise_name', 'token');
+
+// specify message_id for tracing sent messages
+var message_id = 123098;
+
+// specify recipients list in next call
+var sendPageResult = onPageHubApiClient.sendPage(
+    message_id,
+    ['RECIPIENT1', 'RECIPIENT2', ...],
+    'subject text',
+    'body text',
+    function (result) {
+
+    // process result from OnPage Server
+    if (result == null) {
+        console.log('NO RESULT');
+    } else {
+        if (
+            result.SendMessageResult == null ||
+            result.SendMessageResult.Messages == null ||
+            result.SendMessageResult.Messages.Message == null ||
+            result.SendMessageResult.Messages.Message.length < 1
+            ) {
+            console.log('UNKNOWN MESSAGE RESULT');
+        } else {
+            result.SendMessageResult.Messages.Message.forEach(function (message) {
+                console.log('Message: ' + message.id + ' is ' + ((message.ACCEPTED == true) ? 'ACCEPTED' : 'NOT ACCEPTED'));
+            });
+        }
+    }
+});
+
---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+var soap = require('soap');
+
+var OnPageHubApiClient = function (enterprise, token) {
+    this.url = 'https://nps.onpage.com/hub-api?wsdl';
+
+    this.enterprise = enterprise;
+    this.token = token;
+
+    this.credentials = '' + this.enterprise + '+' + this.token;
+};
+
+OnPageHubApiClient.prototype.sendPage = function (message_id, recipients, subject, body, callback) {
+    if (message_id == null) {
+        message_id = 99999; // this is default message_id if the message_id is not specified
+    }
+
+    var recipients_list = [];
+    recipients.forEach(function (recipient) {
+        recipients_list.push({Recipient: { id: recipient}});
+    });
+
+    var args = {
+        Credentials: {
+            Token: this.credentials
+        },
+        Messages: [
+            {
+                Message: {
+                    id: message_id,
+                    Sender: {
+                        ID: 'dispatcher@' + this.enterprise,
+                        type: 'EMAIL'
+                    },
+                    Recipients: recipients_list,
+                    Subject: subject,
+                    Body: body,
+                    Reply: {
+                        AllowFreeText: false
+                    }
+                }
+            }
+        ]
+    };
+
+    function do_callback(result) {
+        if (callback) callback(result);
+    }
+
+    soap.createClient(this.url, function (err, client) {
+        client.SendMessage(args, function (err, result) {
+                if (err) {
+                    console.log('ERROR: ' + err);
+                }
+
+                do_callback(result);
+            }
+        );
+    });
+};
+
+module.exports = OnPageHubApiClient;
